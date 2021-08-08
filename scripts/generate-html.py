dd117f28f6473b4a44bca7d1eadaf95e5bc8d56a
enicholson125 <emn259@gmail.com>
2021-08-08 20:54:39 +0100

    Fix linting errors

--- scripts/generate-html.py
+++ scripts/generate-html.py
@@ -1,5 +1,5 @@
 from jinja2 import Environment, FileSystemLoader
-import helper_functions
+import helper_functions as helper
 
 
 def generate_index():
@@ -7,13 +7,14 @@ def generate_index():
         loader=FileSystemLoader("templates")
     )
     template = env.get_template("cheatsheet.html")
-    # Running actual terraform makes rendering super slow, hence not rendering dynamically
-    # (Plus there's no reason why this can't just be served from a bucket
-    # and that's both easier and more secure so generating the html
-    # beforehand and stashing it in a bucket is what I'm going with)
-    template.globals["get_example_contents"] = helper_functions.get_file_contents
-    template.globals["get_terraform_output"] = helper_functions.get_terraform_output
-    spec = helper_functions.load_cheatsheet_json()
+    # Running actual terraform makes rendering super slow, hence not
+    # rendering dynamically. (Plus there's no reason why this can't
+    # just be served from a bucket and that's both easier and more
+    # secure so generating the html beforehand and stashing it in a
+    # bucket is what I'm going with)
+    template.globals["get_example_contents"] = helper.get_file_contents
+    template.globals["get_terraform_output"] = helper.get_terraform_output
+    spec = helper.load_cheatsheet_json()
     return template.render(spec=spec)
 
 
