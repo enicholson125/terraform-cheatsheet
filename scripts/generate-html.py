825948e06cea7b1f70c0aef7d1994880aae39173
enicholson125 <emn259@gmail.com>
2021-08-21 21:08:28 +0100

    Sanitise terraform for HTML

--- scripts/generate-html.py
+++ scripts/generate-html.py
@@ -2,6 +2,17 @@ from jinja2 import Environment, FileSystemLoader
 import helper_functions as helper
 
 
+def sanitise_for_html(text: str):
+    return text.replace('<', "&lt;").replace('>', "&gt;")
+
+def get_file_contents_and_santitise(filename: str):
+    contents = helper.get_file_contents(filename)
+    return sanitise_for_html(contents)
+
+def get_terraform_output_and_sanitise(filename: str):
+    output = helper.get_terraform_output(filename)
+    return sanitise_for_html(output)
+
 def generate_index():
     env = Environment(
         loader=FileSystemLoader("templates")
@@ -12,8 +23,8 @@ def generate_index():
     # just be served from a bucket and that's both easier and more
     # secure so generating the html beforehand and stashing it in a
     # bucket is what I'm going with)
-    template.globals["get_example_contents"] = helper.get_file_contents
-    template.globals["get_terraform_output"] = helper.get_terraform_output
+    template.globals["get_example_contents"] = get_file_contents_and_santitise
+    template.globals["get_terraform_output"] = get_terraform_output_and_sanitise
     spec = helper.load_cheatsheet_json()
     return template.render(spec=spec)
 
