47cffc07aa62417577f6465d28efefe2cd27a43a
enicholson125 <emn259@gmail.com>
2021-08-21 21:33:33 +0100

    Fix linting

--- scripts/generate-html.py
+++ scripts/generate-html.py
@@ -5,13 +5,16 @@ import helper_functions as helper
 def sanitise_for_html(text: str):
     return text.replace('<', "&lt;").replace('>', "&gt;")
 
+
 def get_file_contents_and_santitise(filename: str):
     contents = helper.get_file_contents(filename)
     return sanitise_for_html(contents)
 
-def get_terraform_output_and_sanitise(filename: str):
+
+def get_tf_output_and_sanitise(filename: str):
     output = helper.get_terraform_output(filename)
     return sanitise_for_html(output)
+
 
 def generate_index():
     env = Environment(
@@ -24,7 +27,7 @@ def generate_index():
     # secure so generating the html beforehand and stashing it in a
     # bucket is what I'm going with)
     template.globals["get_example_contents"] = get_file_contents_and_santitise
-    template.globals["get_terraform_output"] = get_terraform_output_and_sanitise
+    template.globals["get_terraform_output"] = get_tf_output_and_sanitise
     spec = helper.load_cheatsheet_json()
     return template.render(spec=spec)
 
