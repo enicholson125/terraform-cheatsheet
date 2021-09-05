f879edd4ec52519379682338e0987e54f15bcacd
Eleanor Nicholson <eleanor.nicholson@kaluza.com>
2021-09-05 20:03:53 +0100

    Fix commit link in module link example

--- examples/reference_module_git_commit.tf
+++ examples/reference_module_git_commit.tf
@@ -1,7 +1,7 @@
 module "example_string" {
   # Use specifically the version of the module present in the
   # 085ac8443aae62fbe85a21d2abff88464d750e39 commit version in the repo
-  source = "github.com/enicholson125/terraform-cheatsheet.git//example_module?ref=085ac84"
+  source = "github.com/enicholson125/terraform-cheatsheet.git//example_module?ref=72d9d56"
 
   example_string_length = 10
 }
