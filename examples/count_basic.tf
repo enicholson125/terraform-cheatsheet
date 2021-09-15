cf3075301ced8a4e70842bd07c5d171dfe464306
Eleanor Nicholson <eleanor.nicholson@kaluza.com>
2021-09-15 13:43:15 +0100

    Add some details of errors count can be useful to workaround

--- examples/count_basic.tf
+++ examples/count_basic.tf
@@ -2,7 +2,9 @@
 # addresses less readable (e.g. random_string.count_basic[0])
 # will recreate all the resources if you remove the first one in
 # the array (as their index will have changed).
-# However, if you do want to use it:
+# However, if you're hitting errors like:
+# "value depends on resource attributes that cannot be determined until apply"
+# then using count can be a good workaround
 
 variable "string_lengths" {
   default = [2, 5, 1]
