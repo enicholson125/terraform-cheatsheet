09c3e1be6610248a07b78aec68658c85a31a5ab1
enicholson125 <emn259@gmail.com>
2021-10-11 21:43:50 +0100

    Run tfswitch from within the directory

--- scripts/check-terraform-examples.sh
+++ scripts/check-terraform-examples.sh
@@ -7,24 +7,26 @@ function cleanup {
   rm -rf tmp_tf_dir
 }
 
-NO_MAIN_FILE_EXAMPLES="create_module.tfversion_constraint_approx.tfversion_constraint_combo.tfversion_constraint_greater_than.tfversion_constraint_strict.tf"
+NO_MAIN_FILE_EXAMPLES="create_module.tf version_constraint_approx.tf version_constraint_combo.tf version_constraint_greater_than.tf version_constraint_strict.tf"
 
 # TODO script currently has to be run from the top of the repo - fix
 for filename in *.tf; do
     mkdir tmp_tf_dir
     trap cleanup EXIT
 
-    if ! [[ $NO_MAIN_FILE_EXAMPLES =~ .*${filename}.* ]]; then
+    if ! [[ ${NO_MAIN_FILE_EXAMPLES} =~ .*${filename}.* ]]; then
         cp terraform/main.tf tmp_tf_dir/
     fi
     mkdir tmp_tf_dir/example_module
     cp ../example_module/main.tf tmp_tf_dir/example_module/
     cp $filename tmp_tf_dir/
+    pushd tmp_tf_dir
     tfswitch
-    terraform -chdir=tmp_tf_dir init
-    terraform -chdir=tmp_tf_dir fmt -check
-    terraform -chdir=tmp_tf_dir validate
-    terraform -chdir=tmp_tf_dir apply -auto-approve
+    terraform init
+    terraform fmt -check
+    terraform validate
+    terraform apply -auto-approve
+    popd
     # Cleanup
     cleanup
 done
