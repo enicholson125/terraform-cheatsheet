9bc3cd79fb72089061d71a56e594bc2bea38ee96
enicholson125 <emn259@gmail.com>
2021-10-11 21:50:24 +0100

    Use real versions in version examples

--- examples/version_constraint_approx.tf
+++ examples/version_constraint_approx.tf
@@ -1,5 +1,5 @@
 # This allows the right-most number specified to increment
-# so would allow 0.15.1 and 0.15.8 but not 0.15.0
+# so would allow 0.15.1 and 0.15.5 but not 0.15.0
 
 terraform {
   required_version = "~> 0.15.1"
--- examples/version_constraint_combo.tf
+++ examples/version_constraint_combo.tf
@@ -1,7 +1,7 @@
 # You can combine version constraints. For example, this
 # configuration requires a terraform version greater than
-# 0.15.0 but less than or equal to 0.15.8, excluding 0.15.6
+# 0.15.0 but less than or equal to 0.15.5, excluding 0.15.3
 
 terraform {
-  required_version = "> 0.15.0, <= 0.15.8, != 0.15.6"
+  required_version = "> 0.15.0, <= 0.15.5, != 0.15.3"
 }
